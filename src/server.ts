23d3f6eb80dd2404da8a54f4d9dd79a15f7139c7
souravsardar80 <souravsardar@aol.com>
2026-06-04 23:04:34 +0530

    dotenv configure

--- src/server.ts
+++ src/server.ts
@@ -5,16 +5,17 @@ import express, {
   type Response,
 } from "express";
 import { Pool } from "pg";
+import config from "./config/index.js";
 const app: Application = express();
-const port = 3000;
+// const port = 3000;
+const port = config.port;
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
 const pool = new Pool({
-  connectionString:
-    "postgresql://neondb_owner:npg_iIe0yGohD2xj@ep-falling-pine-ap0e9ec4.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require",
+  connectionString: config.connection_string,
 });
 
 const initDB = async () => {
